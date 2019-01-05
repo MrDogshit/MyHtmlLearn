ce8052a2ed6abdfe892fd7528d8c10bd773279c9
HuangLX <1090905255@qq.com>
2019-01-05 23:24:17 +0800

    js学习

---
+++ JavaScriptLearn/Les1.js
@@ -0,0 +1,15 @@
+//原始值: number,boolean,string,undefined,null. 存储位置:栈
+var a = 5;
+var c = false;
+var b = 20.5;
+var str = 'sdfasgerwer';
+var u;
+var n = null;
+f = '1579846';
+document.write(c);
+//引用值: array function Object ... date Regexp  存储位置：堆
+var ar = [1,1.5,3];
+arr = [2];
+arr2 = arr;
+arr.push(5);
+document.write(arr2);
