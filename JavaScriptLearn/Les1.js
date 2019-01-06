4f7f986dc4bf4ff504b4f2d4f420b296c70664d4
HuangLX <1090905255@qq.com>
2019-01-07 00:38:32 +0800

    js课程提交

--- JavaScriptLearn/Les1.js
+++ JavaScriptLearn/Les1.js
@@ -6,10 +6,15 @@ var str = 'sdfasgerwer';
 var u;
 var n = null;
 f = '1579846';
-document.write(c);
+// document.write(c);
 //引用值: array function Object ... date Regexp  存储位置：堆
 var ar = [1,1.5,3];
-arr = [2];
+arr = [2,5,6];
 arr2 = arr;
-arr.push(5);
-document.write(arr2);
+arr = [1,5];
+// arr2指向arr原有的堆空间，arr新赋值后arr有了新的空间，便指向了新的空间
+document.write(arr2);   //arr2 = [2,5,6]
+arr3 = arr;
+arr.push(10);
+// arr3 指向arr原有堆空间，arr push后在原有空间添加了一个数字，所以arr3的数值也跟着变更
+document.write(arr3);
